0be7b7dc62de7eaa8e02d5bfd972c550cf320388
Quinn Peters <quinn.t.peters@gmail.com>
2016-01-19 07:16:34 -0500

    Finished writing tests for error conditions

--- src/errors.js
+++ src/errors.js
@@ -8,13 +8,14 @@ module.exports = (function() {
    }
 
    return {
+		NO_ID: CreateError( "A player must have an ID" ),
       SELF_INJECTION: CreateError( "A Factory cannot inject itself" ),
-      CREATED_NOTHING: CreateError( "Factory didn not create anything" ),
+      CREATED_NOTHING: CreateError( "Factory did not create anything" ),
       MISSING_DEPENDENCY: CreateError( "Factory is missing a dependency" ),
-      ALREADY_INJECTED: CreateError( "Player has already been injected" ),
-      NO_PLAYER_REGISTERED: CreateError( "No such Player has been registered." ),
+      NO_SUCH_PLAYER: CreateError( "No such Player has been registered." ),
       NEEDS_INJECTABLE: CreateError( "Player must define a factory, lib or value" ),
       NOT_PLAYING: CreateError( "Graceland is not playing" ),
-      ALREADY_PLAYING: CreateError( "Graceland is already playing" )
+      ALREADY_PLAYING: CreateError( "Graceland is already playing" ),
+		NO_PLAYERS: CreateError( "Graceland cannot play without players" )
    }
 }());
