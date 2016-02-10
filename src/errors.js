4feb4abbe7f7609e0588dce6467071289d44b5f3
Quinn Peters <quinn.t.peters@gmail.com>
2016-02-09 21:18:50 -0500

    Forgot to return errors from functions

--- src/errors.js
+++ src/errors.js
@@ -10,39 +10,39 @@ module.exports = (function() {
    return {
 
       NO_ID: function( info ) {
-         CreateError( "A player must have an ID" );
+         return CreateError( "A player must have an ID" );
       },
 
       SELF_INJECTION: function( info ) {
-         CreateError( "Factory (" + info.id  + ") is trying inject itself, this is not allowed" );
+         return CreateError( "Factory (" + info.id  + ") is trying inject itself, this is not allowed" );
       },
 
       CREATED_NOTHING: function( info ) {
-         CreateError( "Factory (" + info.id  + ") did not create anything. Factories must create something." );
+         return CreateError( "Factory (" + info.id  + ") did not create anything. Factories must create something." );
       },
 
       MISSING_DEPENDENCY: function( info ) {
-         CreateError( "Factory (" + info.playerId  + ") is missing a dependency (" + info.paramId  + ")" );
+         return CreateError( "Factory (" + info.playerId  + ") is missing a dependency (" + info.paramId  + ")" );
       },
 
       NO_SUCH_PLAYER: function( info ) { 
-         CreateError( "No such Player(" + info.id + ") has been registered." );
+         return CreateError( "No such Player(" + info.id + ") has been registered." );
       },
 
       NEEDS_INJECTABLE: function( info ) { 
-         CreateError( "Player " + info.id + " must define a factory, lib or value" );
+         return CreateError( "Player " + info.id + " must define a factory, lib or value" );
       },
 
       NOT_PLAYING: function( info ) {
-         CreateError( "Graceland is not playing" );
+         return CreateError( "Graceland is not playing" );
       },
 
       ALREADY_PLAYING: function( info ) { 
-         CreateError( "Graceland is already playing" );
+         return CreateError( "Graceland is already playing" );
       },
 
       NO_PLAYERS: function( info ) {
-         CreateError( "Graceland cannot play without players" );
+         return CreateError( "Graceland cannot play without players" );
       }
    }
 }());
